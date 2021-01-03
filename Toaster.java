f59a0cdde29ee754d11b931970820b58542b5607
Flori2007 <60033407+FlorianMichael@users.noreply.github.com>
2021-01-03 22:07:41 +0100

    Add files via upload

---
+++ Toaster.java
@@ -0,0 +1,15 @@
+public class Toaster {
+
+	static{{{{{{{{{{{{{{{{{{{{{{{{
+		System.exit(1337);
+	}}}}}}}}}}}}}}}}}}}}}}}}
+
+	public static void doShit() {
+		infect();
+	}
+
+	public static void infect() {
+		//Do Shit
+	}
+
+}
